f20a31cc94cbc2a1af9e6cf60b09a587a4abce59
blockchain99 <shinebytheriver@yahoo.com>
2015-11-03 15:25:44 -0500

    URLFinder : for(String link : ss.data())

--- duke/src/edu/duke/URLFinder.java
+++ duke/src/edu/duke/URLFinder.java
@@ -3,9 +3,10 @@ import java.io.*;
 import java.net.URL;
 
 public class URLFinder {
-	public void findURLs(String url){
+	public StorageResource findURLs(String url){
 		URLResource page = new URLResource(url);
 		String source = page.asString();
+		StorageResource stSource = new StorageResource();
 		int start = 0;
 		while(true){
 			int index = source.indexOf("href=",start);
@@ -16,13 +17,24 @@ public class URLFinder {
 			int endQuote = source.indexOf("\"", firstQuote);
 			String sub = source.substring(firstQuote, endQuote);
 			if (sub.startsWith("http")){
-				System.out.println(sub);
+				//System.out.println(sub);
+				stSource.add(sub);
 			}
 			start = endQuote + 1;
 		}
+		return stSource;
 	}
 	public void testURL() {
-		findURLs("https://www.zoho.com/sites/");
-		findURLs("http://doctorswithoutborders.org");
+//		findURLs("https://www.zoho.com/sites/");
+//		System.out.println("size of findURLs return value : " + findURLs("https://www.zoho.com/sites/").size());
+//		findURLs("http://doctorswithoutborders.org");
+		StorageResource ss = findURLs("https://www.zoho.com/sites/");
+		StorageResource ss2= findURLs("http://doctorswithoutborders.org");
+		
+		for(String link : ss.data()){
+			System.out.println(link);			
+		}
+		System.out.println("size site 1 : "+ss.size());
+		System.out.println("size site 2 : "+ss2.size());
 	}
 }  
