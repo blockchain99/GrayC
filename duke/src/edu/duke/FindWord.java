a2c9343954f25cd8120c8d9a7936a7dfdf3b6870
blockchain99 <shinebytheriver@yahoo.com>
2015-11-03 11:44:15 -0500

    for(String w : frFW.words()) { store.add(w); }

--- duke/src/edu/duke/FindWord.java
+++ duke/src/edu/duke/FindWord.java
@@ -2,6 +2,10 @@ package edu.duke;
 
 public class FindWord {
 	FileResource frFW = new FileResource("c:\\srcj\\animals.txt");
+	//StorageResource class provides methods for storing and accessing a list of strings of any length. 
+	//Generally you will start by creating an empty StorageResource, 
+	//then adding string values as you find them in a file or web page:
+	StorageResource store = new StorageResource("c:\\srcj\\animalsRes.txt");
 	public void findWisdom(){	
 		int intFW = 0;
 		for(String aline : frFW.lines()){
@@ -26,5 +30,28 @@ public class FindWord {
 			}
 		}
 	}
+	public StorageResource getWords() {
+		for(String w : frFW.words()){
+			store.add(w);
+		}
+		return store;
+	}
+   public void testGetWords(){
+	   for(String item : store.data()){
+		   System.out.println("stored StorageResource size : " + store.size());
+		   if(item.contains("fear")){
+			   System.out.println("found : " + item);
+		   }
+		   else{
+			   System.out.println("There is no word \"fear\" ");
+		   }
+			   
+		   }
+	   store.add("fear");
+	   store.add("fear");
+	   System.out.println("store size after add(): " + store.size());
+	   store.clear();
+	   System.out.println("store size after clear: " + store.size());
+	   }
+   }
 
-}
